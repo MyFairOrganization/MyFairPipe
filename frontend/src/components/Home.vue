e75139bb134c2ee20a86c7cdb7315da6e90ea50d
paulb24 <paul.bankosegger@gmail.com>
2025-11-11 13:08:16 +0100

    login page finished

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { h } from 'vue'
+import { createIMG, createVID } from './Content.vue'
+</script>
+
+<template>
+  <div class="home">
+    <div id="videos">
+      <component
+        v-for="i in 24"
+        :key="i"
+        :is="
+          createIMG(
+            'https://static.vecteezy.com/ti/gratis-vektor/p1/7160087-video-symbol-video-symbol-play-video-zeichen-kostenlos-vektor.jpg',
+            'testVideo' + i,
+          )
+        "
+      />
+    </div>
+  </div>
+</template>
+
+<style>
+.thumbnail {
+  width: 320px;
+  height: 180px;
+  border-radius: 15px;
+}
+
+.image-block:hover {
+  color: #98C1D9;
+}
+
+.video-block,image-block p {
+  text-align: left;
+}
+
+#videos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 40px;
+  width: 100%;
+  align-items: start;
+}
+</style>
---
+++ frontend/src/components/Videoplayer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { createVID } from './Content.vue'
+
+const route = useRoute()
+const props = { id: route.query.id as string, desc: route.query.desc as string }
+</script>
+
+<template>
+  <div class="player">
+    <component :is="createVID(props.id, props.desc)" />
+  </div>
+</template>
+
+<style scoped>
+video {
+  border-radius: 20px;
+}
+</style>
