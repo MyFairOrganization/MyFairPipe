a12ec4fe266a496e909db4269e7143060a8db6e3
htl3r-2135 <2135@htl.rennweg.at>
2025-12-16 10:28:01 +0100

    puts redditor in backend, started on server startup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { startRedditor } from "@/redditor.ts";
 
 const app = createApp(App)
 
